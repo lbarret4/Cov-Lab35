90bf971653f5568de11f9abe2ab275ce8aefdf6f
lbarret4 <llewellynbar4@gmail.com>
2018-10-02 17:05:37 -0400

    added handleEdit

--- client/src/components/BlogEdit.jsx
+++ client/src/components/BlogEdit.jsx
@@ -9,18 +9,32 @@ import * as blogtagsService from '../services/blogtags';
 class BlogEdit extends Component {
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handlesClose = this.handlesClose.bind(this);
         this.state = {
             redirect: false
-
         }
+        this.handlesEdit = this.handlesEdit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handlesClose = this.handlesClose.bind(this);
+    }
+
+
+
+    componentWillReceiveProps() {
+        if (this.props.edit) {
+            console.log("edit", this.props.blog);
+            this.setState({
+                title: this.props.blog.title,
+                content: this.props.blog.content
+                // tags:props.tags;
+            });
+        }
+
     }
 
     handleSubmit(e) {
         alert('submitted post');
         let tags = []
-        let blog = {};    
+        let blog = {};
         for (let item of e.target) {
             if (item.type === 'checkbox' && item.checked && item.name !== 'other') tags.push(item.id);
             if (item.type === 'text' && item.name !== 'otherText') blog.title = item.value;
@@ -38,7 +52,7 @@ class BlogEdit extends Component {
                     blogTag.blogid = id;
                     blogTag.tagid = element;
 
-                    
+
                     let res2 = await blogtagsService.insert(blogTag);
                 });
             } catch (error) {
@@ -54,32 +68,67 @@ class BlogEdit extends Component {
 
     }
 
+    handlesEdit(e) {
+        let target = e.target;
+        if (target.type === 'text') {
+            this.setState({
+                title: target.value
+            });
+        } else if (target.type === 'textarea') {
+            this.setState({
+                title: target.value
+            });
+        }
+    }
+
     handlesClose(e) {
         e.preventDefault();
-        this.props.history.goBack();
+        if (!this.props.edit) {
+            this.props.history.goBack();
+        }
+
     }
 
 
 
     render() {
+        let btn;
+        let titleInput;
+        let textareaInput;
+        if (this.props.edit) {
+            // console.log(this.props.blog);
+            // console.log(this.state);
+            console.log(this.state.title);
+            console.log(this.state.content);
+            btn = <button type="button" className="close" data-toggle="modal" data-target="#blogModal">
+                <span ariaHidden="true" >&times;</span>
+            </button>;
+            titleInput = <input type="text" className="form-control" id="blog_title" placeholder="Blog Title" value={this.state.title} onChange={this.handlesEdit} />;
+            textareaInput = <textarea className="form-control" placeholder="Blog" value={this.state.content} onChange={this.handlesEdit} />;
+
+        } else {
+            btn = <button type="button" className="close" onClick={this.handlesClose}>
+                <span ariaHidden="true">&times;</span>
+            </button>;
+            titleInput = <input type="text" className="form-control" id="blog_title" placeholder="Blog Title" />;
+            textareaInput = <textarea className="form-control" placeholder="Blog" />;
+        }
 
         return (
             <Fragment>
-                {this.state.redirect ? <Redirect to="/" /> :' '}
+                {this.state.redirect ? <Redirect to="/" /> : ' '}
                 <div className="modal-content my-1">
                     <div className="modal-header">
                         <h5 className="modal-title">Post a blog</h5>
-                        <button type="button" className="close" onClick={this.handlesClose}>
-                            <span ariaHidden="true">&times;</span>
-                        </button>
+                        <span>{btn}</span>
                     </div>
                     <div className="modal-body">
                         <form onSubmit={this.handleSubmit}>
                             <div className="form-group">
-                                <input type="text" className="form-control" id="blog_title" placeholder="Blog Title" />
+                                {titleInput}
                             </div>
                             <div className="form-group">
-                                <textarea className="form-control" placeholder="Blog" />
+                                {textareaInput}
 
                             </div>
                             <Tags />
