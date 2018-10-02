1f5a8173251c669a9a04fa85ae68452e45da5ff3
lbarret4 <llewellynbar4@gmail.com>
2018-10-02 02:48:08 -0400

    refactored navbar to use navlinks instead link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,60 +1,78 @@
 import React, { Fragment, Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import AuthButton from './auth/authButton';
 
 class Navbar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            home: 'active',
+            home: '',
             authors: '',
             post: '',
             contact: '',
-
+            login: '',
+            entry: {
+                view:'invisible',
+                path:'/blogs'
+            }
         };
 
 
-        this.handlesOnClick = this.handlesOnClick.bind(this);
+
     }
 
-    componentDidMount() {
-        if (this.props.tab) {
+    setVisible(path) {
+        if (this.state.entry.view === 'invisible') {
             this.setState({
-                home: '',
-                [this.props.tab]: 'active'
-            })
-        }
+                entry: {
+                    view:'visible',
+                    path
+                }
 
+            });
+        }
+    }
+    setInvisible() {
+        if (this.state.entry.view === 'visible') {
+            this.setState({
+                entry:{
+                    view:'invisible',
+                    path:'/blogs'
+                }
+            });
+        }
     }
 
-    handlesOnClick(tab, e) {
 
-        for (let state in this.state) {
-
-            if (this.state[state] === 'active' && state !== tab) {
-                this.setState({
-                    [state]: ' '
-                });
-
-            } else if (state === tab) {
-                this.setState({
-                    [state]: 'active'
-                });
-
-            }
-        }
-
-
-
-    }
 
     render() {
-        let tab = this.props.tab;
-        let tabPath = this.props.path;
+        const visibility = (match,location) => {
+            if (!match) {
+                this.setInvisible();
+                return false;
+            }           
+            this.setVisible(location.pathname);
+            return true;
+        };
+
         let navList = Object.getOwnPropertyNames(this.state).map((st) => {
-            let path = (tab && tabPath && tab === st ? tabPath : `/${st}`);
+            let path = `/${st}`;
+            let link;
+
+            if (st === 'home') {
+                link = (<NavLink exact to='/' className='nav-link' activeClassName='active' >{st[0].toUpperCase() + st.substring(1)}</NavLink>);
+            } else if (st === 'entry') {
+                link = (<NavLink strict to={this.state[st]['path']} className={`nav-link ${this.state[st]['view']}`} activeClassName='active' isActive={visibility} >{st[0].toUpperCase() + st.substring(1)}</NavLink>);
+            } else if (st === "login") {
+
+                link = <AuthButton />;
+
+            } else {
+                link = (<NavLink to={path} className='nav-link' activeClassName='active' >{st[0].toUpperCase() + st.substring(1)}</NavLink>);
+            }
             return (
                 <li className="nav-item">
-                    <Link className={`nav-link ${this.state[st]}`} to={st === 'home' ? '/' : path} onClick={this.handlesOnClick.bind(this, st)}>{st[0].toUpperCase() + st.substring(1)}</Link>
+                    {link}
                 </li>);
         })
 
